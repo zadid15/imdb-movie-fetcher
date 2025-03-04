7053266ae4c2b1e5c5b67e741bf99aef64e19438
zadid15 <muhammadzadid061512@gmail.com>
2025-03-04 15:44:52 +0700

    Improve card and modal UI

--- script.js
+++ script.js
@@ -1,61 +1,62 @@
 // Fetch Refactory
-const searchButton = document.querySelector('.search-button');
-searchButton.addEventListener('click', async function () {
-    try {
-        const inputKeyword = document.querySelector('.input-keyword');
-        const movies = await getMovies(inputKeyword.value);
-        updateUI(movies);
-    } catch (err) {
-        alert(err);
-    }
+const searchButton = document.querySelector(".search-button");
+searchButton.addEventListener("click", async function () {
+  try {
+    const inputKeyword = document.querySelector(".input-keyword");
+    const movies = await getMovies(inputKeyword.value);
+    updateUI(movies);
+  } catch (err) {
+    alert(err);
+  }
 });
 
 function getMovies(keyword) {
-    return fetch('https://www.omdbapi.com/?apikey=641688c2&s=' + keyword)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(response.status);
-            }
-            return response.json();
-        })
-        .then(response => {
-            if (response.Response === "False") {
-                throw new Error(response.Error);
-            }
-            return response.Search;
-        });
+  return fetch("https://www.omdbapi.com/?apikey=641688c2&s=" + keyword)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    })
+    .then((response) => {
+      if (response.Response === "False") {
+        throw new Error(response.Error);
+      }
+      return response.Search;
+    });
 }
 
 function updateUI(movies) {
-    let cards = '';
-    movies.forEach(m => cards += showCards(m));
-    const moviesContainer = document.querySelector('.movie-container');
-    moviesContainer.innerHTML = cards;
+  let cards = "";
+  movies.forEach((m) => (cards += showCards(m)));
+  const moviesContainer = document.querySelector(".movie-container");
+  moviesContainer.innerHTML = cards;
 }
 
 // Event binding untuk tombol detail
-document.addEventListener('click', async function (e) {
-    if (e.target.classList.contains('modal-detail-button')) {
-        const imdbid = e.target.dataset.imdbid;
-        const movieDetail = await getMovieDetail(imdbid);
-        updateUIDetail(movieDetail);
-    }
+document.addEventListener("click", async function (e) {
+  if (e.target.classList.contains("modal-detail-button")) {
+    const imdbid = e.target.dataset.imdbid;
+    const movieDetail = await getMovieDetail(imdbid);
+    updateUIDetail(movieDetail);
+  }
 });
 
 function getMovieDetail(imdbid) {
-    return fetch('https://www.omdbapi.com/?apikey=641688c2&i=' + imdbid)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(response.status);
-            }
-            return response.json();
-        });
+  return fetch("https://www.omdbapi.com/?apikey=641688c2&i=" + imdbid).then(
+    (response) => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    }
+  );
 }
 
 function updateUIDetail(m) {
-    const movieDetail = showMovieDetail(m);
-    const modalBody = document.querySelector('.modal-body');
-    modalBody.innerHTML = movieDetail;
+  const movieDetail = showMovieDetail(m);
+  const modalBody = document.querySelector(".modal-body");
+  modalBody.innerHTML = movieDetail;
 }
 
 function showCards(m) {
@@ -65,15 +66,16 @@ function showCards(m) {
                     <div class="card-body">
                         <h5 class="card-title">${m.Title}</h5>
                         <h6 class="card-subtitle mb-2 text-muted">${m.Year}</h6>
-                        <a href="#" class="btn btn-primary modal-detail-button" data-toggle="modal" 
-                        data-target="#movieDetailModal" data-imdbid="${m.imdbID}">Show Details</a>
+                        <a href="#" class="btn btn-primary modal-detail-button" 
+                        data-bs-toggle="modal" data-bs-target="#movieDetailModal" 
+                        data-imdbid="${m.imdbID}">Show Details</a>
                     </div>
                 </div>
             </div>`;
 }
 
 function showMovieDetail(m) {
-    return `<div class="container-fluid">
+  return `<div class="container-fluid">
                 <div class="row">
                     <div class="col-md-3">
                         <img src="${m.Poster}" class="img-fluid">
